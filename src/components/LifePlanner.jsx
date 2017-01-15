79f8a59a87ac0d988546b3783d2d6092c2265613
Valery Gorodnichev <ValeryG@concur.com>
2017-01-15 15:27:36 -0600

    fixing path on home link

--- src/components/LifePlanner.jsx
+++ src/components/LifePlanner.jsx
@@ -5,7 +5,7 @@ export default ({children}) => (
     <div>
         <h1 className="text-center" style={{color:'red'}}>Life Planner!</h1>
 
-        <Link to="">Home</Link> {' | '}
+        <Link to="home">Home</Link> {' | '}
         <Link to="places">Places To Visit</Link> {' | '}
         <Link to="books">Books To Read</Link> {' | '}
         <Link to="songs">Songs to Listen</Link> {' | '}
