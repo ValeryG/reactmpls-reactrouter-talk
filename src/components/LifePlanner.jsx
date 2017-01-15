f706f62ee0b72dc15927d639ca7b907a4b2b63c2
Valery Gorodnichev <ValeryG@concur.com>
2017-01-15 12:03:19 -0600

    add first react component (LifePlanner.jsx)

---
+++ src/components/LifePlanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+    render() {
+        return (
+            <div>
+                <h1 className="text-center" style={{color:'red'}}>Life Planner!</h1>
+            </div>
+        )
+    }
+})
