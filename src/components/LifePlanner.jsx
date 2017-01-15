ea6ea082c4f3cd855903bc573f16fea7fd89ae06
Valery Gorodnichev <ValeryG@concur.com>
2017-01-15 15:54:36 -0600

    activeStyle on Link

--- src/components/LifePlanner.jsx
+++ src/components/LifePlanner.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router';
+import {Link as OrigLink} from 'react-router';
+
+const Link = (props) => (<OrigLink activeStyle={{fontSize: 16, fontWeight: "bold"}} {...props} />);
 
 export default ({children}) => (
     <div>
         <h1 className="text-center" style={{color:'red'}}>Life Planner!</h1>
 
-        <Link to="home">Home</Link> {' | '}
+        <Link to="home" >Home</Link> {' | '}
         <Link to="places">Places To Visit</Link> {' | '}
         <Link to="books">Books To Read</Link> {' | '}
         <Link to="songs">Songs to Listen</Link> {' | '}
         <Link to="about">About</Link>
-
         <hr />
 
         {children}
