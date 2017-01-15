0dd561f601fcd0b6f3c27733d1ca575e60da5348
Valery Gorodnichev <ValeryG@concur.com>
2017-01-15 12:24:04 -0600

    adding page rendering

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 
 import {places, home} from './api';
+import renderPage from './page';
 
 const app = express();
 
-app.use("/data", express.static("data"));
+app.use('/data', express.static('data'));
+app.get('/api/places', places);
+app.get('/api/home', home);
 
-app.get("/api/places", places);
-app.get("/api/home", home);
+app.get('*', (req, res) => {
+    res
+        .set('Content-Type', 'text/html')
+        .status(200).send(renderPage());
+});
 
-app.listen(3000, () => console.log("listening on port 3000"));
+app.listen(3000, () => console.log('listening on port 3000'));
---
+++ src/server/page.js
@@ -0,0 +1,14 @@
+export default () => {
+    return `
+        <html>
+            <head>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+            </head>
+            <body>
+                <div class="container">
+                    <div id="root"></div>
+                </div>
+            </body>
+        </html>
+    `;
+}
