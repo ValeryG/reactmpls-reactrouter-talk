678e2e9b5e82c36f399024ff765cfc012d734d55
Valery Gorodnichev <ValeryG@concur.com>
2017-01-18 09:55:03 -0600

    fixing main links

--- src/components/LifePlanner.jsx
+++ src/components/LifePlanner.jsx
@@ -7,11 +7,11 @@ export default ({children}) => (
     <div>
         <h1 className="text-center" style={{color:'red'}}>Life Planner!</h1>
 
-        <Link to="home" >Home</Link> {' | '}
-        <Link to="places">Places To Visit</Link> {' | '}
-        <Link to="books">Books To Read</Link> {' | '}
-        <Link to="songs">Songs to Listen</Link> {' | '}
-        <Link to="about">About</Link>
+        <Link to="/home" >Home</Link> {' | '}
+        <Link to="/places">Places To Visit</Link> {' | '}
+        <Link to="/books">Books To Read</Link> {' | '}
+        <Link to="/songs">Songs to Listen</Link> {' | '}
+        <Link to="/about">About</Link>
         <hr />
 
         {children}
